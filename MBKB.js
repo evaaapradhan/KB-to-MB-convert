9213b019bd120d37133627a48d299364c50d1ac5
Evasana <72019274+evaaapradhan@users.noreply.github.com>
2020-11-01 11:50:38 +0545

    Add files via upload

---
+++ MBKB.js
@@ -0,0 +1,3 @@
+const { bytes } = require('./kbtomb.js');
+var con = require('./kbtomb.js');
+con.byte(b=3000);
---
+++ kbtomb.js
@@ -0,0 +1,7 @@
+var kb = {
+    byte: function (bytes){
+        bytes = b/1024;
+        console.log('conversion of KB to Mb :'+ bytes);
+    }
+}
+module.exports = kb
